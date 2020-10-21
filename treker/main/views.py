98573952ddd6015e3666199014ba37260b98a600
Yaroslav <47113131+Yaroher2442@users.noreply.github.com>
2020-10-21 07:32:03 +0300

    design v-0.1

--- treker/main/views.py
+++ treker/main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
-
-
-# Create your views here.
-
-
 def index(request):
-    return render(request, 'main/index.html')
+    context = {'prg_names': [1,2,3,4,4],
+               'page_flag':'',
+               'prg_data':'',
+               'dataset':{},
+               'status':''}
+    return render(request, 'main/index.html', context)
 
 def app(request):
     pass
